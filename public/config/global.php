f162d453d8a5b1e2f6cefd83eede43289f28e1b1
Mitchell M <mitchell.murphy96@gmail.com>
2017-10-31 18:27:59 -0400

    Update with server's database settings

--- public/config/global.php
+++ public/config/global.php
@@ -6,8 +6,8 @@
 
 session_start();
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', '104.236.250.1');
+define('DB_USER', 'project');
+define('DB_PASS', '1231234');
 define('DB_NAME', 'prioritask');
 ?>
