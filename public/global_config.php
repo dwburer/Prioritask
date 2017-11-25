9f3fc6f913722fc2b2d5cef05f789c56e83d4db2
Danny Burer <dburer@morrismultimedia.com>
2017-11-24 23:49:54 -0500

    undo change in global config (we should really be doing stuff like this with env variablesbut oh well)

--- public/global_config.php
+++ public/global_config.php
@@ -4,6 +4,6 @@
 
 // For local dev change to "localhost" - otherwise keep set as the droplet's IP (104.236.250.1)
 // Be mindful of trailing slashes when using.
-define('BASE_URL', 'http://localhost/');
+define('BASE_URL', 'http://104.236.250.1/');
 define('API_URL', BASE_URL . 'api/');
 ?>
